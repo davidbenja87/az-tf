8de65c4b11a2f44953e6b8da41e3f73a460b2ccd
Sugumar <sugumarb@b2b.regn.net>
2021-04-25 06:19:35 +0100

    create rsg

--- resourcegrp.tf
+++ resourcegrp.tf
@@ -1,8 +1,7 @@
 resource "azurerm_resource_group" "bds_dev_purview_rg" {
   name     = "bds-dev-prv-rg"
   location = "Southeast Asia"
-  
-    tags = {
+  tags = {
     environment = "dev"
 	description = "BDS dev resource group"
   }
