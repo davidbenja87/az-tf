5e2a5ade51e78953550341d7f3f14b4214c8fcb4
Sugumar <sugumarb@b2b.regn.net>
2021-04-25 06:17:05 +0100

    create rsg

---
+++ resourcegrp.tf
@@ -0,0 +1,10 @@
+resource "azurerm_resource_group" "bds_dev_purview_rg" {
+  name     = "bds-dev-prv-rg"
+  location = "Southeast Asia"
+  
+    tags = {
+    environment = "dev"
+	description = "BDS dev resource group"
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "GITHUB_AZ_SUB" {
+  description = "Azure subscription"
+  default = "974668b8-821d-4cc2-a84d-c81a7733f464"
+  
+}
+
