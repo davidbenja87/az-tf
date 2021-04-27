6ead6f30318d4592b1d84504caa1e4c3c6cbc60c
davidbenja87 <38088886+davidbenja87@users.noreply.github.com>
2021-04-27 06:35:21 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -28,3 +28,15 @@ provider "azurerm" {
     client_secret   = "iyIeh.i_KapdTX7YW5f3xXNv-2.K8Pwz5l"
     tenant_id       = "c8fedfb4-d804-457b-9745-362581de2e4c"
 }
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "githubtfstate"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = "XxuN2vFEbkB0wGSH9wkyzhA4Wkph0hF/JwPJ/mONXLw43VpTUG3evqeyJQOyp/cb+AyhPhh7C3R14XJl6oa7vQ=="
+  }
+}
